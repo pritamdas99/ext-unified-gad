4a29916753c444aeff9ad0b5c937d54f1487d4a6
pritamdas99 <pd170299@gmail.com>
2026-02-10 00:35:46 +0600

    changes

--- src/e2e_model.py
+++ src/e2e_model.py
@@ -162,21 +162,10 @@ class UnifyMLPDetector(object):
     
     @torch.no_grad()
     def eval(self, labels, probs):
-        result = []
-        for t in range(len(probs)):
-            probs_t = probs[t]
-            labels_t = labels[t]
-            result_t = {}
-            for k in self.output_route:
-                result_t[k] = self._single_eval(labels_t[k], probs_t[k])
-            result.append(result_t)
-
-        final_result = {k: {'MacroF1':0, 'AUROC':0, 'AUPRC':0} for k in self.output_route}
+        result = {}
         for k in self.output_route:
-            final_result[k]['MacroF1'] = sum([r[k]['MacroF1'] for r in result]) / len(result)
-            final_result[k]['AUROC'] = sum([r[k]['AUROC'] for r in result]) / len(result)
-            final_result[k]['AUPRC'] = sum([r[k]['AUPRC'] for r in result]) / len(result)
-        return final_result
+            result[k] = self._single_eval(labels[k], probs[k])
+        return result
 
     def train(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.args.lr_ft, weight_decay=self.args.l2_ft)
@@ -204,6 +193,7 @@ class UnifyMLPDetector(object):
                 loss = torch.stack(loss).mean() # average the loss of different tasks
                 result = {k: sum(d[k] for d in loss_items) / len(loss_items) for k in loss_items[0]}
 
+                print(f"[DEBUG AFTER LOSS] loss={loss} loss_items={loss_items} logits_dict={logits_dict[:70]}")
 
                 for k in loss_items_total_train:
                     loss_items_total_train[k] += result[k]
@@ -229,8 +219,8 @@ class UnifyMLPDetector(object):
             torch.cuda.empty_cache()
             
             with torch.no_grad():
-                labels_dict_val_mul = []
-                probs_dict_val_mul = []
+                labels_dict_val_mul = {k:[] for k in self.output_route }
+                probs_dict_val_mul = {k:[] for k in self.output_route }
                 loss_items_total_val = {k:0 for k in self.output_route }
                 # eval loop
                 for batched_data in self.val_dataloader:
@@ -240,13 +230,24 @@ class UnifyMLPDetector(object):
                     batched_khop_graph = batched_data['sp_matrices']
                     # FIXME: device issue?
                     batched_graph = [graph.to(self.args.device) for graph in batched_graph]
-                    for t,label_dict in enumerate(batched_labels_dict):
-                        labels_dict_val_mul_t = {k:[] for k in self.output_route }
-                        for k,v in label_dict.items():
-                            batched_labels_dict[t][k] = v.to(self.args.device)
-                            if k[0] in self.output_route:
-                                labels_dict_val_mul_t[k[0]].append(v)
-                        labels_dict_val_mul.append(labels_dict_val_mul_t)
+                    
+                    for k in batched_labels_dict[0]:
+                        labels_mul_t=[]
+                        kk=NAME_MAP[k]
+                        for t,label_dict in enumerate(batched_labels_dict):
+                            if kk[0] in self.output_route:
+                                labels_mul_t.append(label_dict[kk])
+                        labels_dict_val_mul[k].append(labels_mul_t.to(self.args.device))
+                            
+                    
+                    # for t,label_dict in enumerate(batched_labels_dict):
+                    #     labels_dict_val_mul_t = {k:[] for k in self.output_route }
+                    #     for k,v in label_dict.items():
+                    #         batched_labels_dict[t][k] = v.to(self.args.device)
+                    #         if k[0] in self.output_route:
+                    #             labels_dict_val_mul_t[k[0]].append(v)
+                    #     labels_dict_val_mul.append(labels_dict_val_mul_t)
+                    
                     batched_khop_graph = [graph.to(self.args.device) for graph in batched_khop_graph]
                     
                     self.model.eval()
@@ -260,11 +261,11 @@ class UnifyMLPDetector(object):
                         #     loss_items_total_val[k] += loss_items[k]
 
                         probs = self.get_probs(logits_dict)
-                        for t, prob_t in enumerate(probs):
-                            probs_dict_val_mul_t = {k:[] for k in self.output_route }
-                            for k in prob_t:
-                                probs_dict_val_mul_t[k].append(prob_t[k])
-                            probs_dict_val_mul.append(probs_dict_val_mul_t)
+                        for k in probs[0]:
+                            probs_mul_t=[]
+                            for t, prob_t in enumerate(probs):
+                                probs_mul_t.append(prob_t[k])
+                            probs_dict_val_mul[k].append(probs_mul_t)
                     
                     del batched_data
                     del batched_graph
@@ -273,10 +274,9 @@ class UnifyMLPDetector(object):
                     del logits_dict
                     del probs
                 with torch.no_grad():
-                    for t in range(len(labels_dict_val_mul)):
-                        for k in self.output_route:
-                            labels_dict_val_mul[t][k] = torch.cat([t for t in labels_dict_val_mul[t][k]])
-                            probs_dict_val_mul[t][k] = torch.cat([t for t in probs_dict_val_mul[t][k]])
+                    for k in self.output_route:
+                        labels_dict_val_mul[k] = torch.cat([t for t in labels_dict_val_mul[k]], dim=1)
+                        probs_dict_val_mul[k] = torch.cat([t for t in probs_dict_val_mul[k]], dim=1)
                     # get eval score
                     score_val = self.eval(labels_dict_val_mul, probs_dict_val_mul)
                     del labels_dict_val_mul
