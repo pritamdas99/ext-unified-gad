eb6530988ac0c055cb9ae7f235b8c2d16c2170eb
pritamdas99 <pd170299@gmail.com>
2026-02-10 18:41:01 +0600

    changes

--- src/e2e_model.py
+++ src/e2e_model.py
@@ -298,7 +298,7 @@ class UnifyMLPDetector(object):
                         for k in probs[0]:
                             probs_mul_t=[]
                             for t, prob_t in enumerate(probs):
-                                probs_mul_t.append(prob_t[k])
+                                probs_mul_t.append(prob_t[k].tolist())
                             probs_mul_t = pad_to_rectangle(probs_mul_t)
                             if not probs_dict_val_mul[k[0]]:
                                 probs_dict_val_mul[k[0]] = torch.tensor(probs_mul_t)
